968effbf9c3c19e9b85cb706664bb37e073175ad
Sebastiaan van Stijn <github@gone.nl>
2026-03-18 13:27:53 +0100

    touch-up godoc
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- sgr.go
+++ sgr.go
@@ -4,101 +4,102 @@ import (
 	"fmt"
 )
 
-// RGB3Bit is a 3bit RGB color.
+// RGB3Bit is a 3-bit RGB color.
 type RGB3Bit uint8
 
-// RGB8Bit is a 8bit RGB color.
+// RGB8Bit is an 8-bit indexed color.
 type RGB8Bit uint8
 
 func newSGR(n uint) ANSI {
 	return newAnsi(fmt.Sprintf(esc+"%dm", n))
 }
 
-// NewRGB3Bit create a RGB3Bit from given RGB.
+// NewRGB3Bit returns a 3-bit color from the given RGB values.
 func NewRGB3Bit(r, g, b uint8) RGB3Bit {
 	return RGB3Bit((r >> 7) | ((g >> 6) & 0x2) | ((b >> 5) & 0x4))
 }
 
-// NewRGB8Bit create a RGB8Bit from given RGB.
+// NewRGB8Bit returns an 8-bit indexed color from the given RGB values.
 func NewRGB8Bit(r, g, b uint8) RGB8Bit {
 	return RGB8Bit(16 + 36*(r/43) + 6*(g/43) + b/43)
 }
 
-// Color3BitF set the foreground color of text.
+// Color3BitF returns an SGR sequence that sets the foreground color.
 func Color3BitF(c RGB3Bit) ANSI {
 	return newAnsi(fmt.Sprintf(esc+"%dm", c+30))
 }
 
-// Color3BitB set the background color of text.
+// Color3BitB returns an SGR sequence that sets the background color.
 func Color3BitB(c RGB3Bit) ANSI {
 	return newAnsi(fmt.Sprintf(esc+"%dm", c+40))
 }
 
-// Color8BitF set the foreground color of text.
+// Color8BitF returns an SGR sequence that sets the 8-bit foreground color.
 func Color8BitF(c RGB8Bit) ANSI {
 	return newAnsi(fmt.Sprintf(esc+"38;5;%dm", c))
 }
 
-// Color8BitB set the background color of text.
+// Color8BitB returns an SGR sequence that sets the 8-bit background color.
 func Color8BitB(c RGB8Bit) ANSI {
 	return newAnsi(fmt.Sprintf(esc+"48;5;%dm", c))
 }
 
-// FullColorF set the foreground color of text.
+// FullColorF returns an SGR sequence that sets the 24-bit foreground color.
 func FullColorF(r, g, b uint8) ANSI {
 	return newAnsi(fmt.Sprintf(esc+"38;2;%d;%d;%dm", r, g, b))
 }
 
-// FullColorB set the foreground color of text.
+// FullColorB returns an SGR sequence that sets the 24-bit background color.
 func FullColorB(r, g, b uint8) ANSI {
 	return newAnsi(fmt.Sprintf(esc+"48;2;%d;%d;%dm", r, g, b))
 }
 
-// Style
+// Text styles and decorations, represented as Select Graphic Rendition (SGR)
+// parameters as defined by ECMA-48 / ISO 6429.
 var (
-	// Bold set the text style to bold or increased intensity.
+	// Bold sets the text style to bold or increased intensity.
 	Bold ANSI = newSGR(1)
 
-	// Faint set the text style to faint.
+	// Faint sets the text style to faint or decreased intensity.
 	Faint ANSI = newSGR(2)
 
-	// Italic set the text style to italic.
+	// Italic sets the text style to italic.
 	Italic ANSI = newSGR(3)
 
-	// Underline set the text style to underline.
+	// Underline sets the text style to underlined.
 	Underline ANSI = newSGR(4)
 
-	// BlinkSlow set the text style to slow blink.
+	// BlinkSlow sets the text style to slow blinking.
 	BlinkSlow ANSI = newSGR(5)
 
-	// BlinkRapid set the text style to rapid blink.
+	// BlinkRapid sets the text style to rapid blinking.
 	BlinkRapid ANSI = newSGR(6)
 
-	// Inverse swap the foreground color and background color.
+	// Inverse swaps the foreground and background colors.
 	Inverse ANSI = newSGR(7)
 
-	// Conceal set the text style to conceal.
+	// Conceal sets the text style to concealed.
 	Conceal ANSI = newSGR(8)
 
-	// CrossOut set the text style to crossed out.
+	// CrossOut sets the text style to crossed-out.
 	CrossOut ANSI = newSGR(9)
 
-	// Frame set the text style to framed.
+	// Frame sets the text style to framed.
 	Frame ANSI = newSGR(51)
 
-	// Encircle set the text style to encircled.
+	// Encircle sets the text style to encircled.
 	Encircle ANSI = newSGR(52)
 
-	// Overline set the text style to overlined.
+	// Overline sets the text style to overlined.
 	Overline ANSI = newSGR(53)
 )
 
-// Foreground color of text.
+// Foreground colors, represented as SGR parameters.
 var (
-	// DefaultF is the default foreground color.
+	// DefaultF restores the default foreground color.
 	DefaultF ANSI = newSGR(39)
 
-	// Normal color
+	// Standard foreground colors.
 	BlackF   ANSI = newSGR(30)
 	RedF     ANSI = newSGR(31)
 	GreenF   ANSI = newSGR(32)
@@ -108,7 +109,7 @@ var (
 	CyanF    ANSI = newSGR(36)
 	WhiteF   ANSI = newSGR(37)
 
-	// Light color
+	// Bright foreground colors.
 	LightBlackF   ANSI = newSGR(90)
 	LightRedF     ANSI = newSGR(91)
 	LightGreenF   ANSI = newSGR(92)
@@ -119,12 +120,12 @@ var (
 	LightWhiteF   ANSI = newSGR(97)
 )
 
-// Background color of text.
+// Background colors, represented as SGR parameters.
 var (
-	// DefaultB is the default background color.
+	// DefaultB restores the default background color.
 	DefaultB ANSI = newSGR(49)
 
-	// Normal color
+	// Standard background colors.
 	BlackB   ANSI = newSGR(40)
 	RedB     ANSI = newSGR(41)
 	GreenB   ANSI = newSGR(42)
@@ -134,7 +135,7 @@ var (
 	CyanB    ANSI = newSGR(46)
 	WhiteB   ANSI = newSGR(47)
 
-	// Light color
+	// Bright background colors.
 	LightBlackB   ANSI = newSGR(100)
 	LightRedB     ANSI = newSGR(101)
 	LightGreenB   ANSI = newSGR(102)
