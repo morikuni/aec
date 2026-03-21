989925abf9b55abce4fae4fba9e82c662e64ed0e
Sebastiaan van Stijn <github@gone.nl>
2026-03-21 13:03:54 +0100

    godoc: document per line
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- sgr.go
+++ sgr.go
@@ -57,41 +57,21 @@ func FullColorB(r, g, b uint8) ANSI {
 // Text styles and decorations, represented as Select Graphic Rendition (SGR)
 // parameters as defined by ECMA-48 / ISO 6429.
 var (
-	// Bold sets the text style to bold or increased intensity.
-	Bold ANSI = newSGR(1)
+	// Intensity and text presentation.
+	Bold       ANSI = newSGR(1) // enables bold or increased intensity.
+	Faint      ANSI = newSGR(2) // enables faint or decreased intensity.
+	Italic     ANSI = newSGR(3) // enables italic.
+	Underline  ANSI = newSGR(4) // enables underline.
+	BlinkSlow  ANSI = newSGR(5) // enables slow blinking.
+	BlinkRapid ANSI = newSGR(6) // enables rapid blinking.
+	Inverse    ANSI = newSGR(7) // swaps the foreground and background colors.
+	Conceal    ANSI = newSGR(8) // conceals the text.
+	CrossOut   ANSI = newSGR(9) // crosses out the text.
 
-	// Faint sets the text style to faint or decreased intensity.
-	Faint ANSI = newSGR(2)
-
-	// Italic sets the text style to italic.
-	Italic ANSI = newSGR(3)
-
-	// Underline sets the text style to underlined.
-	Underline ANSI = newSGR(4)
-
-	// BlinkSlow sets the text style to slow blinking.
-	BlinkSlow ANSI = newSGR(5)
-
-	// BlinkRapid sets the text style to rapid blinking.
-	BlinkRapid ANSI = newSGR(6)
-
-	// Inverse swaps the foreground and background colors.
-	Inverse ANSI = newSGR(7)
-
-	// Conceal sets the text style to concealed.
-	Conceal ANSI = newSGR(8)
-
-	// CrossOut sets the text style to crossed-out.
-	CrossOut ANSI = newSGR(9)
-
-	// Frame sets the text style to framed.
-	Frame ANSI = newSGR(51)
-
-	// Encircle sets the text style to encircled.
-	Encircle ANSI = newSGR(52)
-
-	// Overline sets the text style to overlined.
-	Overline ANSI = newSGR(53)
+	// Framing and overlines.
+	Frame    ANSI = newSGR(51) // enables framing.
+	Encircle ANSI = newSGR(52) // enables encircling.
+	Overline ANSI = newSGR(53) // enables overline.
 )
 
 // Foreground colors, represented as SGR parameters.
@@ -100,24 +80,24 @@ var (
 	DefaultF ANSI = newSGR(39)
 
 	// Standard foreground colors.
-	BlackF   ANSI = newSGR(30)
-	RedF     ANSI = newSGR(31)
-	GreenF   ANSI = newSGR(32)
-	YellowF  ANSI = newSGR(33)
-	BlueF    ANSI = newSGR(34)
-	MagentaF ANSI = newSGR(35)
-	CyanF    ANSI = newSGR(36)
-	WhiteF   ANSI = newSGR(37)
+	BlackF   ANSI = newSGR(30) // black.
+	RedF     ANSI = newSGR(31) // red.
+	GreenF   ANSI = newSGR(32) // green.
+	YellowF  ANSI = newSGR(33) // yellow.
+	BlueF    ANSI = newSGR(34) // blue.
+	MagentaF ANSI = newSGR(35) // magenta.
+	CyanF    ANSI = newSGR(36) // cyan.
+	WhiteF   ANSI = newSGR(37) // white (gray).
 
 	// Bright foreground colors.
-	LightBlackF   ANSI = newSGR(90)
-	LightRedF     ANSI = newSGR(91)
-	LightGreenF   ANSI = newSGR(92)
-	LightYellowF  ANSI = newSGR(93)
-	LightBlueF    ANSI = newSGR(94)
-	LightMagentaF ANSI = newSGR(95)
-	LightCyanF    ANSI = newSGR(96)
-	LightWhiteF   ANSI = newSGR(97)
+	LightBlackF   ANSI = newSGR(90) // bright black.
+	LightRedF     ANSI = newSGR(91) // bright red.
+	LightGreenF   ANSI = newSGR(92) // bright green.
+	LightYellowF  ANSI = newSGR(93) // bright yellow.
+	LightBlueF    ANSI = newSGR(94) // bright blue.
+	LightMagentaF ANSI = newSGR(95) // bright magenta.
+	LightCyanF    ANSI = newSGR(96) // bright cyan.
+	LightWhiteF   ANSI = newSGR(97) // bright white.
 )
 
 // Background colors, represented as SGR parameters.
@@ -126,22 +106,22 @@ var (
 	DefaultB ANSI = newSGR(49)
 
 	// Standard background colors.
-	BlackB   ANSI = newSGR(40)
-	RedB     ANSI = newSGR(41)
-	GreenB   ANSI = newSGR(42)
-	YellowB  ANSI = newSGR(43)
-	BlueB    ANSI = newSGR(44)
-	MagentaB ANSI = newSGR(45)
-	CyanB    ANSI = newSGR(46)
-	WhiteB   ANSI = newSGR(47)
+	BlackB   ANSI = newSGR(40) // black.
+	RedB     ANSI = newSGR(41) // red.
+	GreenB   ANSI = newSGR(42) // green.
+	YellowB  ANSI = newSGR(43) // yellow.
+	BlueB    ANSI = newSGR(44) // blue.
+	MagentaB ANSI = newSGR(45) // magenta.
+	CyanB    ANSI = newSGR(46) // cyan.
+	WhiteB   ANSI = newSGR(47) // white (gray).
 
 	// Bright background colors.
-	LightBlackB   ANSI = newSGR(100)
-	LightRedB     ANSI = newSGR(101)
-	LightGreenB   ANSI = newSGR(102)
-	LightYellowB  ANSI = newSGR(103)
-	LightBlueB    ANSI = newSGR(104)
-	LightMagentaB ANSI = newSGR(105)
-	LightCyanB    ANSI = newSGR(106)
-	LightWhiteB   ANSI = newSGR(107)
+	LightBlackB   ANSI = newSGR(100) // bright black.
+	LightRedB     ANSI = newSGR(101) // bright red.
+	LightGreenB   ANSI = newSGR(102) // bright green.
+	LightYellowB  ANSI = newSGR(103) // bright yellow.
+	LightBlueB    ANSI = newSGR(104) // bright blue.
+	LightMagentaB ANSI = newSGR(105) // bright magenta.
+	LightCyanB    ANSI = newSGR(106) // bright cyan.
+	LightWhiteB   ANSI = newSGR(107) // bright white.
 )
